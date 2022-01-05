b86f7c69de0727c4e4fbe9938f848d9e78d20f03
fumtas1k <fumta01a7@gmail.com>
2022-01-05 22:43:05 +0900

    update:ルーティングを整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/new'
-  get 'blogs/confirm'
-  get 'blogs/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "blogs#index"
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
