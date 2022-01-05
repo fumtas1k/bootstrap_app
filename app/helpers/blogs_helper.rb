a208426943bf7f2c00e6a91d4d3cb385aaf60400
fumtas1k <fumta01a7@gmail.com>
2022-01-05 23:19:56 +0900

    add:経過時間表示用メソッド作成しindexに導入

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,13 @@
 module BlogsHelper
+  def relative_post_datetime(created_at)
+    sec = (Time.zone.now - created_at).round
+    days = sec / (60 * 60 * 24)
+    days_format = created_at.strftime("%m月%d日%")
+    return days_format unless days.zero?
+    hours = sec / (60 * 60)
+    return "#{hours}時間" unless hours.zero?
+    mins = sec / (60)
+    return "#{mins}分" unless mins.zero?
+    "#{sec}"
+  end
 end
