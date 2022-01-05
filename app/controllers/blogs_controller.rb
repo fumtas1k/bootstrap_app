be6e46c7738684b777aa3e3db38396dc96ab7c92
fumtas1k <fumta01a7@gmail.com>
2022-01-05 23:44:31 +0900

    add:新規投稿機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,9 +8,19 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
 
   def confirm
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "投稿しました!"
+    else
+      render :new
+    end
   end
 
   def edit
