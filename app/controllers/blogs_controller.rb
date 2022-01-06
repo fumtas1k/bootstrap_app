f0a4c8b457bc3cd3e76d028aacbdd0069209a32c
fumtas1k <fumta01a7@gmail.com>
2022-01-06 10:48:53 +0900

    add:削除機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -34,6 +34,11 @@ class BlogsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "削除しました!"
+  end
+
   private
     def blog_params
       params.require(:blog).permit(:title, :content)
